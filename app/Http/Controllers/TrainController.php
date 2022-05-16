3a82024a2d28192f8edc049c5ddbf030a1e05af3
giagra91 <giagra91@gmail.com>
2022-05-16 08:30:14 +0200

    update function createNewTrain in TrainController

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -13,4 +13,24 @@ class TrainController extends Controller
 
         return view('trains.index', ["trains" => $trains]);
     }
+
+    public function createNewTrain($agency, $departure_station, $arrival_station, $departrure_date, $departure_time, $arrival_date,
+    $arrival_time, $train_code, $carriages_number, $is_in_time,$is_deleted){
+        $newTrain = new Train();
+        $newTrain->fill([
+            "agency" => $agency,
+            "departure_station" => $departure_station,
+            "arrival_station" => $arrival_station,
+            "departrure_date" => $departrure_date,
+            "departure_time" => $departure_time,
+            "arrival_date" => $arrival_date,
+            "arrival_time" => $arrival_time,
+            "train_code" => $train_code,
+            "carriages_number" => $carriages_number,
+            "is_in_time" => $is_in_time,
+            "is_deleted" => $is_deleted,
+        ]);
+
+        $newTrain->save();
+    }
 }
--- app/Train.php
+++ app/Train.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    protected $fillable = [
+        "agency",
+        "departure_station",
+        "arrival_station",
+        "departrure_date",
+        "departure_time",
+        "arrival_date",
+        "arrival_time",
+        "train_code",
+        "carriages_number",
+        "is_in_time",
+        "is_deleted"
+    ];
 }
