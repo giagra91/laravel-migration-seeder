8533303e60639321543697f90bcbee95619c0e8f
giagra91 <giagra91@gmail.com>
2022-05-16 08:35:38 +0200

    indentation change

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -9,13 +9,14 @@ class TrainController extends Controller
 {
     public function index(){
         $date = "2022-05-13";
-        $trains = Train::whereDate("departure_date", ">=" , $date)->get();
+        $trains = Train::whereDate("departure_date", ">=" , $date)
+        ->get();
 
         return view('trains.index', ["trains" => $trains]);
     }
 
     public function createNewTrain($agency, $departure_station, $arrival_station, $departrure_date, $departure_time, $arrival_date,
-    $arrival_time, $train_code, $carriages_number, $is_in_time,$is_deleted){
+    $arrival_time, $train_code, $carriages_number, $is_in_time, $is_deleted){
         $newTrain = new Train();
         $newTrain->fill([
             "agency" => $agency,
